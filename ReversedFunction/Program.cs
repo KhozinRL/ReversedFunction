e991b76845de5776631b09734716e127be5f1896
Р.Л. Хозин <s190291019@live.cs.msu.ru>
2021-02-11 11:18:37 +0300

    Add project files.

---
+++ ReversedFunction/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace ReversedFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double power = -4;
+            Double epsilon = Math.Pow(10, power);
+            ReversedFunction.AccuracyUpdated += Console.WriteLine;
+
+            //Делегат
+            Func<double, double> sinDelegate = Math.Sin;
+            double sinResult = ReversedFunction.reversedFunction((0.1, 1.3), sinDelegate, 0.5, epsilon);
+            Console.WriteLine("При x = {0:f5} функция sin(x) = 0.5 \n", sinResult);
+
+            //Анонимный метод
+            Func<double, double> secondDelegate = delegate (double x) { return x * x + Math.Sin(x - 2); };
+            double secondResult = ReversedFunction.reversedFunction((2.5, 3.5), secondDelegate, 8, epsilon);
+            Console.WriteLine("При x = {0:f5} функция x*x + sin(x-2) = 8 \n", secondResult);
+
+            //Лямбда
+            double expResult = ReversedFunction.reversedFunction((-3, 0), x => Math.Exp(-x), 9, epsilon);
+            Console.WriteLine("При x = {0:f5} функция exp(-x) = 9 \n", expResult);
+        }
+ 
+    }
+}
---
+++ ReversedFunction/ReversedFunction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReversedFunction
+{
+
+    class ReversedFunction
+    {
+        static public event Action<string, object> AccuracyUpdated;
+
+        public static double reversedFunction((double a, double b) boundaries, Func<double, double> function, double y, double epsilon)
+        {
+            AccuracyUpdated?.Invoke("Достигнутая точность: {0:f6}", boundaries.b - boundaries.a);
+
+            if (Math.Abs(boundaries.a - boundaries.b) <= epsilon)
+                return (boundaries.a + boundaries.b) / 2;
+
+            double center = (boundaries.b + boundaries.a) / 2;
+
+            Boolean raising = function(boundaries.b) - function(boundaries.a) > 0 ? true : false;
+
+            if ((function(center) < y && raising == false) || (function(center) > y && raising))
+            {
+                return reversedFunction((boundaries.a, center), function, y, epsilon);
+            }
+            else
+            {
+                return reversedFunction((center, boundaries.b), function, y, epsilon);
+            }
+        }
+    }
+}
